a6a1af1582a2506ba7565f1f6392c8154156861f
ZT <zt8479@gmail.com>
2017-10-16 22:47:10 +0800

    完成用户部分接口

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
@@ -22,11 +21,9 @@ class UserController extends Controller
                 'name' => $request->name,
             ]);
         } catch (\Exception $exception) {
-//            return response()->json(['code' => 100, 'msg' => '注册失败']);
-            return $this->error_response('注册失败');
+            return response()->json(['code' => 100, 'msg' => '注册失败']);
         }
-//        return response()->json(['code' => 0, 'msg' => '注册成功']);
-        return $this->array_response('注册成功');
+        return response()->json(['code' => 0, 'msg' => '注册成功']);
     }
 
     public function login(Request $request)
@@ -37,10 +34,10 @@ class UserController extends Controller
         ]);
         $user = User::where('email', $request->email)->first();
         if (!password_verify($request->password, $user->password))
-            return $this->error_response('密码错误');
+            return response()->json(['code' => 100, 'msg' => '密码错误']);
         else {
             $token = \JWTAuth::fromUser($user);
-            return $this->array_response('登陆成功')->withHeaders(['authorization' => 'Bearer ' . $token]);
+            return response()->json(['code' => 0, 'msg' => '登陆成功'])->withHeaders(['authorization' => 'Bearer ' . $token]);
         }
     }
 
@@ -55,39 +52,27 @@ class UserController extends Controller
         $this->validate($request, [
             'id' => 'required|integer|exists:users',
         ]);
-        $user = User::where('id', $request->id)->first();
-        return $user;
+           $user = User::where('id', $request->id)->first();
+            return $user;
+            //todo 没有值的时候，返回“无此用户”
     }
-
-    // todo 修改用户信息
-
-
     public function update(Request $request)
     {
         $this->validate($request, [
-            'email' => 'required_without_all:password,name|email|unique:users',
-            'password' => 'required_without_all:email,name|string|min:6|max:20',
-            'name' => 'required_without_all:email,password|string|min:2|max:20'
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6|max:20',
+            'name' => 'required|string|min:2|max:20'
         ]);
-        log::info('all',[$request->all()]);
-
-        $input = $request->only('email','password','name');
-        if ($request->has('password')) {
-            $input['password'] = password_hash($input['password'], PASSWORD_DEFAULT);
-        }
-
-        $user = $request->user();
         try {
-//            $user = User::update(['email' => $request->email,'password' => password_hash($request->password, PASSWORD_DEFAULT),'name' => $request->name,]);
-            foreach ($input as $key => $value) {
-                $user->$key = $value;
-            }
-            $user->save();
+            $user = User::update([
+                'email' => $request->email,
+                'password' => password_hash($request->password, PASSWORD_DEFAULT),
+                'name' => $request->name,
+            ]);
         } catch (\Exception $exception) {
-            log::info('msg',[$exception]);
-            return $this->error_response('修改失败');
+            return response()->json(['code' => 100, 'msg' => '修改失败']);
         }
-        return $this->array_response(['user' => $user]);
+        return response()->json(['code' => 0, 'msg' => '修改成功'.$user]);
     }
 
 }
